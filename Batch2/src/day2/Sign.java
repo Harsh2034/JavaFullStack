66c4f921d9d32e1f5e213a6632a9400d6d1ce28a
Harsh Gupta <hsgupta716@gmail.com>
2025-12-27 15:43:01 +0530

    other main method

---
+++ Batch2/src/day2/Sign.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Sign {
+	 public static void main(String [] args) {
+		System.out.println("Hello");
+		Sign.main(7);
+		int ar[]= {10,20,30,40};
+		Sign.main(ar);
+	}
+	 public static void main(int a) {
+		 System.out.println("accepting any arg"+a);
+	 }
+	 public static void main(int [] b) {
+		System.out.println("another arg " +b[2]);
+	}
+	
+}
