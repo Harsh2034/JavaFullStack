507f382b8bc0af06dcc1c773a008d38eb09cde07
Harsh Gupta <hsgupta716@gmail.com>
2025-12-27 12:09:01 +0530

    test

---
+++ Batch2/src/day2/AreaOfRect.java
@@ -0,0 +1,32 @@
+package day2;
+
+class Rectangle{
+	int height;
+	int base;
+	static double  a = 0.5;
+	
+	
+	Rectangle(int height, int base){
+		this.base = base;
+		this.height = height;
+	}
+	
+	double display() {
+		return a*base*height;
+	}
+}
+
+public class AreaOfRect {
+
+	public static void main(String[] args) {
+		int height = 5;
+		int base = 8;
+		Rectangle r  = new Rectangle(height, base);
+		System.out.println(r.display());
+		
+		
+		
+
+	}
+
+}
